67c269a12bd76a641082c712194d69fb5391618d
caldeira54 <caldeiraa54@gmail.com>
2022-10-01 16:16:50 -0300

    Implementação de uma classe com as cores que seram utilizadas e início da tela de login

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <title>Página Inicial</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <?php
-        
-    ?>
-  </body>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tela Inicial</title>
+  <link rel="stylesheet" href="./visao/css/estilo.css">
+</head>
+<body>
+  <form>
+    <div class='formLogin'>
+      <h4>Login</h4>
+
+      <label>Usuário</label>
+      <hr>
+
+      <label>Senha</label>
+    </div>
+  </form>
+</body>
 </html>
---
+++ visao/css/cores.php
@@ -0,0 +1,17 @@
+<?php
+class Cores
+{
+    switch($cor){
+        case 'preto':
+            return '#2a0308';
+        case 'marrom':
+            return '#924f1b';
+        case 'amarelo':
+            return '#e2ac3f';
+        case 'bege':
+            return '#f8ebbe';
+        case 'ciano':
+            return '#7ba58d';
+    }
+}
+?>
