e6a20071abd5c314514ac107dc09834ecf226996
caldeira54 <caldeiraa54@gmail.com>
2022-11-08 20:14:41 -0300

    Criação de uma popup de teste

--- index.php
+++ index.php
@@ -5,10 +5,26 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./visao/css/estilo.css">
   <title>Tela Inicial</title>
 </head>
 
 <body>
+  <button>Clique aqui</button>
+
+  <div class="popup-wrapper">
+    <div class="popup">
+      <div class="popup-close">x</div>
+      <div class="popup-content">
+        <h2>Popup</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <a class="popup-link" href="#">Saiba mais</a>
+      </div>
+    </div>
+  </div>
+
+  <script src="./popup.js"></script>
+
   <a href="visao/fornecedor/listagem.php">Listagem de Fornecedor</a>
   <br>
   <a href="visao/fornecedor/formCadastro.php">Cadastrar Fornecedor</a>
@@ -48,6 +64,7 @@
   <a href="visao/vendaAVista/listagem.php">Listagem das Vendas à Vista</a>
   <br>
   <a href="visao/vendaAVista/formCadastro.php">Cadastrar Venda à Vista</a>
+
 </body>
 
 </html>
