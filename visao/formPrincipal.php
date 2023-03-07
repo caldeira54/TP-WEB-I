bd7d4de5e9edbaf41bbe86585acb92ca51ce9a0c
caldeira54 <caldeiraa54@gmail.com>
2023-03-06 22:40:46 -0300

    Adição do bootstrap na tela de cadastro de fornecdores

--- visao/formPrincipal.php
+++ visao/formPrincipal.php
@@ -5,15 +5,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <!-- Icones -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous">
   </script>
   <link rel="stylesheet" href="./estilo.css">
+  <link rel="stylesheet" href="../sass/main.css">
   <title>Página Principal</title>
 </head>
 
-<body>
+<body class="body">
   <?php
   session_start();
   if(!$_SESSION['usuario'])
@@ -24,15 +26,18 @@
   
   include('./menu.php');
   ?>
-  <div class="funcionario">
-    <p >
+  <div class="rounded float-end text-center">
+    <p class="card-text">
       <?php
       echo 'Bem vindo: ';
       echo $_SESSION['nome'];
       ?>
     </p>
-    <a href="../logout.php"><img src="./css/imagens/sair.png"></a>
+
+    <a href="../logout.php"><img src="./css/imagens/sair.png" class="rounded float-end"></a>
   </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- visao/fornecedor/formCadastro.php
+++ visao/fornecedor/formCadastro.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/estilo.css">
+    <!-- <link rel="stylesheet" href="../css/estilo.css"> -->
+    <link rel="stylesheet" href="../../sass/main.css">
     <link rel="stylesheet" href="../estilo.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
@@ -18,30 +19,36 @@ include('../menu.php');
 ?>
 
 <body class="body">
-    <div class="cadastro">
-        <h1>Cadastro de Fornecedores</h1>
-        <br>
-        <form action="cadastro.php" method="post">
-            <label for="nome">Fornecedor</label>
-            <input type="text" name="nome" id="nome">
+    <div class="container-fluid text-center" style="width: 36rem;">
+        <div class="card" style="padding-top: 20px;">
+            <h1 class="card-title text-center">Cadastro de Fornecedores</h1>
 
-            <br>
+            <form action="cadastro.php" method="post" class="mb-5 row justify-content-md-center">
+                <label class="col-sm-4 col-form-label" for="nome">Fornecedor</label>
+                <div class="col-sm-5">
+                    <input class="form-control mb-3" type="text" name="nome" id="nome">
+                </div>
 
-            <label for="endereco">Endereço</label>
-            <input type="text" name="endereco" id="endereco">
+                <label class="col-sm-4 col-form-label" for="endereco">Endereço</label>
+                <div class="col-sm-5">
+                    <input class="form-control mb-3" type="text" name="endereco" id="endereco">
+                </div>
 
-            <br>
+                <div class="col-sm-5">
+                    <button class="btn btn-outline-success">Salvar</button>
+                </div>
+            </form>
 
-            <button class="btnSalvar">Salvar</button>
-        </form>
+            <div class="row justify-content-md-center mb-4">
+                <form action="../formPrincipal.php" class="col-5 text-center">
+                    <button class="btn btn-outline-dark"> Início </button>
+                </form>
 
-        <form action="../formPrincipal.php">
-            <button class="btnInicio"> Início </button>
-        </form>
-
-        <form action="./listagem.php">
-            <button> Listagem </button>
-        </form>
+                <form action="./listagem.php" class="col-5 text-center">
+                    <button class="btn btn-outline-dark"> Listagem </button>
+                </form>
+            </div>
+        </div>
     </div>
     <script type="text/javascript" src="../script.js"></script>
 </body>
